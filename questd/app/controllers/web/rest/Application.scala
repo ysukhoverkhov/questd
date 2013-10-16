d4a5770f35a3376cd6ce18364b4ed3198ba3d582
Yury <Yury@localhost>
2013-10-16 16:13:49 +0400

    Login form created.

--- questd/app/controllers/web/rest/Application.scala
+++ questd/app/controllers/web/rest/Application.scala
@@ -3,12 +3,11 @@ package controllers.web.rest
 import play.api._
 import play.api.mvc._
 
-
 object Application extends Controller {
 
   def index = Action {
-//    Redirect("http://google.com/", 302)
-    Ok("This is our super index. it should redirect somewhere.")
+    Redirect("http://google.com/")
+    //    Ok("This is our super index. it should redirect somewhere.")
   }
 
 }
--- questd/app/controllers/web/testclient/Client.scala
+++ questd/app/controllers/web/testclient/Client.scala
@@ -3,13 +3,42 @@ package controllers.web.testclient
 import play.api._
 import play.api.mvc._
 
-object Client extends Controller{
+import play.api.data._
+import play.api.data.Forms._
 
-  def login = Action {
-    Ok("Client login/register")
+object Client extends Controller {
+
+  def index = Action {
+    Redirect(routes.Client.login)
   }
 
-  def register = TODO
-  
+  case class LoginCredentials(name: String, pass: String)
+  val loginCredentialsForm = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "pass" -> nonEmptyText)(LoginCredentials.apply)(LoginCredentials.unapply))
+
+  def login = Action {
+    Ok(views.html.testclient.login(loginCredentialsForm))
+  }
+
+  def loginTarget = Action { implicit request =>
+    loginCredentialsForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.testclient.login(formWithErrors))
+      },
+      userData => {
+        //    val newUser = models.User(userData.name, userData.age)
+        //    val id = models.User.create(newUser)
+        //    Redirect(routes.Application.home(id))
+        Ok
+      })
+
+  }
+
+  def main = Action {
+    Ok("Main client screen")
+  }
+
 }
 
