20f94282ce044e841637d2650ca37e01f8a964fd
Yury <Yury@localhost>
2013-10-16 16:33:20 +0400

    Registration form completed.

--- questd/app/controllers/web/testclient/Client.scala
+++ questd/app/controllers/web/testclient/Client.scala
@@ -18,14 +18,22 @@ object Client extends Controller {
       "name" -> nonEmptyText,
       "pass" -> nonEmptyText)(LoginCredentials.apply)(LoginCredentials.unapply))
 
+  case class RegisterCredentials(name: String, pass1: String, pass2: String)
+  val registerCredentialsForm = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "pass1" -> nonEmptyText,
+      "pass2" -> nonEmptyText)(RegisterCredentials.apply)(RegisterCredentials.unapply) verifying("Passwords are not the same", (x) => x.pass1 == x.pass2)
+      )
+      
   def login = Action {
-    Ok(views.html.testclient.login(loginCredentialsForm))
+    Ok(views.html.testclient.login(loginCredentialsForm, registerCredentialsForm))
   }
 
   def loginTarget = Action { implicit request =>
     loginCredentialsForm.bindFromRequest.fold(
       formWithErrors => {
-        BadRequest(views.html.testclient.login(formWithErrors))
+        BadRequest(views.html.testclient.login(formWithErrors, registerCredentialsForm))
       },
       userData => {
         //    val newUser = models.User(userData.name, userData.age)
@@ -36,6 +44,21 @@ object Client extends Controller {
 
   }
 
+  def registerTarget = Action { implicit request =>
+    registerCredentialsForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.testclient.login(loginCredentialsForm, formWithErrors))
+      },
+      userData => {
+        //    val newUser = models.User(userData.name, userData.age)
+        //    val id = models.User.create(newUser)
+        //    Redirect(routes.Application.home(id))
+        Ok
+      })
+
+  }
+  
+  
   def main = Action {
     Ok("Main client screen")
   }
