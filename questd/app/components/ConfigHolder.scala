8fd3fa525d37eadf53fb697963b053ce86531ec4
Yury <Yury@localhost>
2013-11-05 12:32:14 +0400

    Cleaned up.

--- questd/app/components/ConfigHolder.scala
+++ questd/app/components/ConfigHolder.scala
@@ -5,21 +5,31 @@ import controllers.domain._
 import controllers.domain.admin._
 
 trait ConfigHolder { this: APIAccessor =>
-  
-  // Name of config section we use to store our configuration.
+
+  /**
+   *  Name of config section we use to store our configuration.
+   */ 
   def configSectionName: String
-  
+
+  /**
+   * Default configuration if empty or should be reset.
+   */
   def defaultConfiguration: ConfigSection
-  
+
   def config: ConfigSection = {
     api.getConfigSection(GetConfigSectionRequest(configSectionName)) match {
       case OkApiResult(Some(GetConfigSectionResult(Some(c: ConfigSection)))) => c
-      case _ => {
-        api.setConfigSection(SetConfigSectionRequest(defaultConfiguration))
-        defaultConfiguration
-      }
-    } 
+      case _ => resetConfig
+    }
   }
-  
+
+  /**
+   * Resets component's configuration to default one.
+   */
+  def resetConfig = {
+    api.setConfigSection(SetConfigSectionRequest(defaultConfiguration))
+    defaultConfiguration
+  }
+
 }
 
