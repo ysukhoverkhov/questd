61aa9d0bef4d59aa3135f18b241ad3ff391a13cf
Yury <Yury@localhost>
2014-06-16 20:12:07 +0400

    Quests crud implemented.

--- questd/app/controllers/web/admin/component/QuestsCRUDImpl.scala
+++ questd/app/controllers/web/admin/component/QuestsCRUDImpl.scala
@@ -12,12 +12,44 @@ import controllers.domain._
 import controllers.domain.admin._
 import components._
 
+case class QuestForm(
+  id: String,
+  status: String,
+  level: Int,
+  difficulty: String,
+  duration: String)
+
 trait QuestsCRUDImpl extends Controller { this: APIAccessor =>
+
+  val form = Form(
+    mapping(
+      "id" -> text,
+      "status" -> nonEmptyText,
+      "level" -> number,
+      "difficulty" -> nonEmptyText,
+      "duration" -> nonEmptyText)(QuestForm.apply)(QuestForm.unapply))
 
   /**
    * Get all quests
    */
   def quests(id: String) = Action { implicit request =>
+
+    // Filling form.
+    val f = if (id == "") {
+      form
+    } else {
+      api.getQuestAdmin(GetQuestAdminRequest(id)) match {
+        case OkApiResult(Some(GetQuestAdminResult(Some(quest)))) => {
+          form.fill(QuestForm(
+            id = quest.id,
+            status = quest.status.toString,
+            level = quest.info.level,
+            difficulty = quest.info.difficulty.toString,
+            duration = quest.info.duration.toString))
+        }
+        case _ => form
+      }
+    }
 
     // Filling table.
     api.allQuests(AllQuestsRequest()) match {
@@ -25,7 +57,8 @@ trait QuestsCRUDImpl extends Controller { this: APIAccessor =>
       case OkApiResult(Some(a: AllQuestsResult)) => Ok(
         views.html.admin.quests(
           Menu(request),
-          a.quests.toList))
+          a.quests.toList,
+          f))
 
       case _ => Ok("Internal server error - themes not received.")
     }
@@ -34,22 +67,37 @@ trait QuestsCRUDImpl extends Controller { this: APIAccessor =>
   /**
    * Updates quest status by request from CRUD
    */
-  def updateQuest(id: String) = Action { implicit request =>
+  def updateQuest = Action { implicit request =>
+    form.bindFromRequest.fold(
 
-    val values: Map[String, String] = request.body.asFormUrlEncoded match {
-      case Some(m) => {
-        m.map((e) => e match {
-          case (k, v) => (k -> v.head)
-        })
-      }
-      case _ => Map()
-    }
-    
-    Logger.error(values.toString)
+      formWithErrors => {
+        
+        Logger.error(formWithErrors.errors.toString)
+        
+        BadRequest(views.html.admin.quests(
+          Menu(request),
+          List(),
+          formWithErrors))
+      },
 
-    Redirect(controllers.web.admin.routes.QuestsCRUD.quests())
+      questForm => {
+
+        if (questForm.id == "") {
+          //          val theme = Theme(id = "", text = themeForm.text, comment = themeForm.comment)
+          //          api.createTheme(CreateThemeRequest(theme))
+        } else {
+          api.updateQuestAdmin(UpdateQuestAdminRequest(
+              questForm.id,
+              questForm.status, 
+              questForm.level,
+              questForm.difficulty,
+              questForm.duration))
+        }
+
+        Redirect(controllers.web.admin.routes.QuestsCRUD.quests(questForm.id))
+      })
   }
- 
-  
+
 }
+
 
