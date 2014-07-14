a14a4f3917235b8100c9f0b5d63ccfa2c55d1fb8
Yury <Yury@localhost>
2014-07-14 18:39:51 +0400

    Tests fixed.

--- questd/test/controllers/domain/app/user/SolveQuestAPISpecs.scala
+++ questd/test/controllers/domain/app/user/SolveQuestAPISpecs.scala
@@ -73,105 +73,100 @@ class SolveQuestAPISpecs extends BaseAPISpecs {
 
     "Create regular solution for regular users" in context {
 
-//      val u = createUser(vip = false)
-//      val s = createSolutionInfoContent
-//
-//      user.resetQuestSolution(any, any) returns Some(u)
-//
-//      val result = api.proposeSolution(ProposeSolutionRequest(u, s))
-//
-//      result.body.get.allowed must beEqualTo(ProfileModificationResult.OK)
-//
-//      there was one(solution).create(
-//        QuestSolution(
-//          id = anyString,
-//          userId = u.id,
-//          questLevel = u.profile.questSolutionContext.takenQuest.get.obj.level,
-//          info = QuestSolutionInfo(
-//            content = s,
-//            themeId = u.profile.questSolutionContext.takenQuest.get.obj.themeId,
-//            questId = u.profile.questSolutionContext.takenQuest.get.id,
-//            vip = false),
-          voteEndDate = any))
-//          voteEndDate = any))
+      val u = createUser(vip = false)
+      val s = createSolutionInfoContent
+
+      user.resetQuestSolution(any, any) returns Some(u)
+
+      val result = api.proposeSolution(ProposeSolutionRequest(u, s))
+
+      result.body.get.allowed must beEqualTo(ProfileModificationResult.OK)
+
+      there was one(solution).create(
+        QuestSolution(
+          id = anyString,
+          userId = u.id,
+          questLevel = u.profile.questSolutionContext.takenQuest.get.obj.level,
+          info = QuestSolutionInfo(
+            content = s,
+            themeId = u.profile.questSolutionContext.takenQuest.get.obj.themeId,
+            questId = u.profile.questSolutionContext.takenQuest.get.id,
+            vip = false),
           voteEndDate = new Date()))
-      success
-          voteEndDate = any))
+    }
+    
+    "Create VIP solution for VIP users" in context {
+
+      val u = createUser(vip = true)
+      val s = createSolutionInfoContent
+
+      user.resetQuestSolution(any, any) returns Some(u)
+
+      val result = api.proposeSolution(ProposeSolutionRequest(u, s))
+
+      result.body.get.allowed must beEqualTo(ProfileModificationResult.OK)
+
+      there was one(solution).create(
+        QuestSolution(
+          id = anyString,
+          userId = u.id,
+          questLevel = u.profile.questSolutionContext.takenQuest.get.obj.level,
+          info = QuestSolutionInfo(
+            content = s,
+            themeId = u.profile.questSolutionContext.takenQuest.get.obj.themeId,
+            questId = u.profile.questSolutionContext.takenQuest.get.id,
+            vip = true),
           voteEndDate = new Date()))
-      
+    }
+
+    "Do not fight with himself in quest" in context {
+
+      val user1 = createUser("user1")
+      val mySolution = createSolution("solId1", user1.id, "qid")
+
+      solution.allWithParams(
+        status = Some(QuestSolutionStatus.WaitingForCompetitor.toString),
+        questIds = List(mySolution.info.questId)) returns List(mySolution).iterator
+
+      val result = api.tryFightQuest(TryFightQuestRequest(mySolution))
+
+      result must beEqualTo(OkApiResult(TryFightQuestResult()))
+
+      there were no(solution).updateStatus(any, any)
+      there were no(user).storeSolutionInDailyResult(any, any)
+    }
+
+    "Receive reward for winning quest battle" in context {
+
+      val quest = createQuest("qid")
+      val user1 = createUser("user1")
+      val mySolution = createSolution("solId1", user1.id, quest.id, points = 1)
+      val user2 = createUser("user2")
+      val rivalSolution = createSolution("solId2", user2.id, quest.id, points = 0)
+
+      solution.allWithParams(
+        status = Some(QuestSolutionStatus.WaitingForCompetitor.toString),
+        questIds = List(mySolution.info.questId)) returns List(mySolution, rivalSolution).iterator
+
+      solution.updateStatus(mySolution.id, QuestSolutionStatus.Won.toString) returns Some(mySolution.copy(status = QuestSolutionStatus.Won))
+      solution.updateStatus(rivalSolution.id, QuestSolutionStatus.Lost.toString) returns Some(rivalSolution.copy(status = QuestSolutionStatus.Lost))
+
+      user.readById(mySolution.userId) returns Some(user1)
+      user.readById(rivalSolution.userId) returns Some(user2)
+
       db.user.storeSolutionInDailyResult(Matchers.eq(user1.id), any) returns Some(user1)
       db.user.storeSolutionInDailyResult(Matchers.eq(user2.id), any) returns Some(user2)
+    
+      db.quest.readById(quest.id) returns Some(quest)
+
+      val result = api.tryFightQuest(TryFightQuestRequest(mySolution))
+
+      result must beEqualTo(OkApiResult(TryFightQuestResult()))
+
+      there were two(solution).updateStatus(any, any)
+      there were two(user).readById(any)
+      there were two(user).storeSolutionInDailyResult(any, any)
     }
-// TODO fix the test.
-//    "Create VIP solution for VIP users" in context {
-//
-//      val u = createUser(vip = true)
-//      val s = createSolutionInfoContent
-//
-//      user.resetQuestSolution(any, any) returns Some(u)
-//
-//      val result = api.proposeSolution(ProposeSolutionRequest(u, s))
-//
-//      result.body.get.allowed must beEqualTo(ProfileModificationResult.OK)
-//
-//      there was one(solution).create(
-//        QuestSolution(
-//          id = anyString,
-//          userId = u.id,
-//          questLevel = u.profile.questSolutionContext.takenQuest.get.obj.level,
-//          info = QuestSolutionInfo(
-//            content = s,
-//            themeId = u.profile.questSolutionContext.takenQuest.get.obj.themeId,
-//            questId = u.profile.questSolutionContext.takenQuest.get.id,
-//            vip = true),
-//          voteEndDate = any))
-//    }
-//
-//    "Do not fight with himself in quest" in context {
-//
-//      val user1 = createUser("user1")
-//      val mySolution = createSolution("solId1", user1.id, "qid")
-//
-//      solution.allWithParams(
-//        status = Some(QuestSolutionStatus.WaitingForCompetitor.toString),
-//        questIds = List(mySolution.info.questId)) returns List(mySolution).iterator
-//
-//      val result = api.tryFightQuest(TryFightQuestRequest(mySolution))
-//
-//      result must beEqualTo(OkApiResult(TryFightQuestResult()))
-//
-//      there were no(solution).updateStatus(any, any)
-//      there were no(user).storeSolutionInDailyResult(any, any)
-//    }
-//
-//    "Receive reward for winning quest battle" in context {
-//
-//      val quest = createQuest("qid")
-//      val user1 = createUser("user1")
-//      val mySolution = createSolution("solId1", user1.id, quest.id, points = 1)
-//      val user2 = createUser("user2")
-//      val rivalSolution = createSolution("solId2", user2.id, quest.id, points = 0)
-//
-//      solution.allWithParams(
-//        status = Some(QuestSolutionStatus.WaitingForCompetitor.toString),
-//        questIds = List(mySolution.info.questId)) returns List(mySolution, rivalSolution).iterator
-//
-//      solution.updateStatus(mySolution.id, QuestSolutionStatus.Won.toString) returns Some(mySolution.copy(status = QuestSolutionStatus.Won))
-//      solution.updateStatus(rivalSolution.id, QuestSolutionStatus.Lost.toString) returns Some(rivalSolution.copy(status = QuestSolutionStatus.Lost))
-//
-//      user.readById(mySolution.userId) returns Some(user1)
-//      user.readById(rivalSolution.userId) returns Some(user2)
-//
-//      db.quest.readById(quest.id) returns Some(quest)
-//
-//      val result = api.tryFightQuest(TryFightQuestRequest(mySolution))
-//
-//      result must beEqualTo(OkApiResult(TryFightQuestResult()))
-//
-//      there were two(solution).updateStatus(any, any)
-//      there were two(user).readById(any)
-//      there were two(user).storeSolutionInDailyResult(any, any)
-//    }
 
   }
 
