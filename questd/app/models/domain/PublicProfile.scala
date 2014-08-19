c059edb5a00aa3f8ac5a3afe24f0d9978938bdda
Yury <Yury@localhost>
2014-08-19 10:06:01 +0400

    id added to public profile.

--- questd/app/models/domain/PublicProfile.scala
+++ questd/app/models/domain/PublicProfile.scala
@@ -1,12 +1,15 @@
 package models.domain
 
+import models.domain.base.ID
+
 /**
  * This can be given to client as is, thus contains only public information.
  */
 case class PublicProfile(
+  publicProfileId: String = ID.generateUUID(),
   publicProfileVersion: Int = 1,
   level: Int = 18, // Should be 0 here.
   bio: Bio = Bio(),
   vip: Boolean = false,
-  debug: String = "")
+  debug: String = "") // TODO: remove me in 0.20.2
 
