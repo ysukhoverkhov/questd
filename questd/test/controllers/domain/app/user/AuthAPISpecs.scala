4ee0b698bee98af2babbfdcba72bf62bdfa08e1a
Yury <Yury@localhost>
2014-08-28 09:37:32 +0400

    Tests cleaned up.

--- questd/test/controllers/domain/app/user/AuthAPISpecs.scala
+++ questd/test/controllers/domain/app/user/AuthAPISpecs.scala
@@ -1,18 +1,9 @@
 package controllers.domain.app.user
 
-import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
-import play.api.test._
-import play.api.test.Helpers._
-import controllers.domain._
-import controllers.domain.app.user._
-import models.store._
-import models.domain._
-import models.store.mongo._
-import controllers.domain.InternalErrorApiResult
-import controllers.domain.NotAuthorisedApiResult
+import controllers.domain.{InternalErrorApiResult, NotAuthorisedApiResult, _}
 import controllers.sn.client.SNUser
+import models.domain._
+import models.store._
 
 
 class AuthAPISpecs extends BaseAPISpecs {
@@ -22,24 +13,27 @@ class AuthAPISpecs extends BaseAPISpecs {
     "Register user with new FB id" in context {
 
       val fbid = "fbid"
+      val candc = "country_name"
+
       val userfb = mock[SNUser]
       userfb.snId returns fbid
-      
-      val u = Some(User("", AuthInfo(snids = Map("FB" -> fbid))))
-        
+
+      val u = Some(User("", AuthInfo(snids = Map("FB" -> fbid)), demo = UserDemographics(cultureId = candc)))
+
       db.user.readBySNid("FB", fbid) returns None thenReturns u
       db.user.levelup(anyString, anyInt) returns Some(User("", AuthInfo(snids = Map("FB" -> fbid))))
       db.user.setNextLevelRatingAndRights(
         anyString,
         anyInt,
         any) returns Some(User("", AuthInfo(snids = Map("FB" -> fbid))))
-      
+      db.culture.findByCountry("") returns Some(Culture(id = candc, name = candc))
+
       val rv = api.login(LoginRequest("FB", userfb))
 
       // Update allowed.
-      there were two(user).readBySNid("FB", fbid) 
-//      there were one(user).create(u.get)
-//      there were one(user).update(u.get)
+      there were two(user).readBySNid("FB", fbid)
+      //      there were one(user).create(u.get)
+      //      there were one(user).update(u.get)
 
       rv must beAnInstanceOf[OkApiResult[LoginResult]]
       rv.body must beSome[LoginResult]
@@ -48,10 +42,16 @@ class AuthAPISpecs extends BaseAPISpecs {
     "Login existing user with new FB id" in context {
 
       val fbid = "fbid"
+      val candc = "country_name"
+
       val userfb = mock[SNUser]
 
       userfb.snId returns fbid
-      db.user.readBySNid("FB", fbid) returns Some(User("", AuthInfo(snids = Map("FB" -> fbid))))
+      db.user.readBySNid("FB", fbid) returns Some(User(
+        "",
+        auth = AuthInfo(snids = Map("FB" -> fbid)),
+        demo = UserDemographics(cultureId = candc)))
+      db.culture.findByCountry("") returns Some(Culture(id = candc, name = candc))
 
       val rv = api.login(LoginRequest("FB", userfb))
 
