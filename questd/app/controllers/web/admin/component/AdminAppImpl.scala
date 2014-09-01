e70eacc6c4ee9a95c76ffb7bd83bcbcce6f86a16
Yury <Yury@localhost>
2014-09-01 18:47:47 +0400

    Cleaned up a bit.

--- questd/app/controllers/web/admin/component/AdminAppImpl.scala
+++ questd/app/controllers/web/admin/component/AdminAppImpl.scala
@@ -5,12 +5,11 @@ import play.api.mvc._
 
 class AdminAppImpl(val api: DomainAPIComponent#DomainAPI) extends Controller with SecurityAdminImpl {
 
-  def index = Authenticated {implicit request => 
+  def index = Authenticated { implicit request =>
     Ok(views.html.admin.index(Menu(request)))
   }
-  
-  def login = Action {implicit request => 
+
+  def login = Action { implicit request =>
     storeAuthInfoInResult(Ok(views.html.admin.index(Menu(request))))
   }
 }
-
