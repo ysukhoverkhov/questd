f8873fc6351451f003d27140293b797b7062c4cc
Yury <Yury@localhost>
2014-10-02 09:34:42 +0400

    Code cleaned up.

--- questd/app/controllers/web/rest/component/DailyResultWSImpl.scala
+++ questd/app/controllers/web/rest/component/DailyResultWSImpl.scala
@@ -1,14 +1,8 @@
 package controllers.web.rest.component
 
-import play.api._
-import play.api.mvc._
 import controllers.domain.app.user._
-import controllers.domain._
 import controllers.web.rest.component.helpers._
-import controllers.web.rest.component._
 import controllers.web.rest.protocol._
-import models.domain._
-import org.json4s._
 
 trait DailyResultWSImpl extends QuestController with SecurityWSImpl with CommonFunctions { this: WSComponent#WS =>
 
