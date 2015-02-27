30292e818d4323616a7e854491b4b5cd5479eaaf
Yury <Yury@localhost>
2015-02-27 19:36:04 +0300

    SolutionListInfo is removed and replaced with SolitionView.

--- questd/app/models/domain/view/SolutionListInfo.scala
+++
@@ -1,8 +0,0 @@
-package models.domain.view
-
-// TODO: remove me since it's obsolete with SolutionView
-case class SolutionListInfo (
-  solution: SolutionView,
-  quest: Option[QuestView],
-  author: Option[ProfileView]
-  )
