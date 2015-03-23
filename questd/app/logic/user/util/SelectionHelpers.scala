a9ddffb73942e50ab36abd751d9be0da73e3a836
Yury <Yury@localhost>
2015-03-23 14:21:47 +0300

    Comments added.

--- questd/app/logic/user/util/SelectionHelpers.scala
+++ questd/app/logic/user/util/SelectionHelpers.scala
@@ -78,7 +78,7 @@ trait SelectionHelpers {/* this: UserLogic =>*/
   }
 
   private[util] def levelsForNextObjectWithSigmaDistribution(mean: Float, sigma: Float, rand: RandomComponent#Random): (Int, Int) = {
-
+// TODO: test me.
     val v0 = rand.nextGaussian(mean, sigma)
     val v1 = Math.round(v0).toInt
     val v2 = if (v1 < 1) 1 else if (v1 > constants.MaxLevel) constants.MaxLevel else v1
