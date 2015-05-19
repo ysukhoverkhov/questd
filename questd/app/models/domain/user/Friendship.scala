1bae53b27822cc8b31fbb10e3cb82f146dec43b5
Yury <Yury@localhost>
2015-05-19 13:03:54 +0300

    Model updated to store  who refers whom now.

--- questd/app/models/domain/user/Friendship.scala
+++ questd/app/models/domain/user/Friendship.scala
@@ -1,8 +1,10 @@
 package models.domain.user
 
+
 /**
  * Represents friendship status
  */
 case class Friendship(
   friendId: String,
-  status: FriendshipStatus.Value)
+  status: FriendshipStatus.Value,
+  referralStatus: ReferralStatus.Value = ReferralStatus.None)
