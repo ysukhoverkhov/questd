d68eca11caa515541cbf8f092a5a265bab02aa9f
Yury <Yury@localhost>
2015-06-22 10:06:44 +0300

    Migration script updated.

--- db_migration/migrate_0.40.05_0.40.06.js
+++ db_migration/migrate_0.40.05_0.40.06.js
@@ -9,3 +9,14 @@ db.users.find().forEach(
    }
 )
 
+
+db.users.find().forEach(
+    function(doc) {
+
+	doc.profile.assets = { "coins" : NumberInt("0"), "money" : NumberInt("0") , "rating" : NumberInt("0")};
+	doc.profile.publicProfile.level = "1";
+        doc.profile.rights.unlockedFunctionality = [ "VoteQuests", "VoteSolutions", "VoteBattles", "VoteReviews", "AddToFollowing"];
+
+        db.users.save(doc);
+   }
+)
