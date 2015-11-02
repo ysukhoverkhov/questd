b796ad8bbe5597953280c73ba689e2a06d0eb190
Yury <Yury@localhost>
2015-11-02 17:01:40 +0300

    push notifications uncommented in test.

--- questd/app/controllers/web/rest/component/DebugWSImpl.scala
+++ questd/app/controllers/web/rest/component/DebugWSImpl.scala
@@ -7,7 +7,8 @@ import controllers.domain.admin.{AllQuestsRequest, AllSolutionsRequest, AllUsers
 import controllers.domain.app.quest.VoteQuestRequest
 import controllers.domain.app.solution.VoteSolutionRequest
 import controllers.domain.app.user._
-import controllers.services.devicenotifications.{DeviceNotificationsComponent, InactiveDevices}
+import controllers.services.devicenotifications.DeviceNotifications.IOSDevice
+import controllers.services.devicenotifications.{DeviceNotifications, DeviceNotificationsComponent, InactiveDevices}
 import controllers.web.helpers._
 import models.domain.common.{ContentReference, ContentType, ContentVote}
 import models.domain.quest.QuestInfoContent
@@ -77,13 +78,13 @@ trait DebugWSImpl extends BaseController with SecurityWSImpl with CommonFunction
 
   def test = wrapApiCallReturnBody[WSDebugResult] { r =>
 
-//    c.actor ! DeviceNotifications.PushMessage(
-//      devices = DeviceNotifications.Devices(Set(IOSDevice("250bad8f be421ebf 716da622 7680bbc3 3cf333e9 ec11a625 487176f6 895bd207"))),
-//      message = "lalala",
-//      badge = None,
-//      sound = None,
-//      destinations = List(DeviceNotifications.MobileDestination)
-//    )
+    c.actor ! DeviceNotifications.PushMessage(
+      devices = DeviceNotifications.Devices(Set(IOSDevice("250bad8f be421ebf 716da622 7680bbc3 3cf333e9 ec11a625 487176f6 895bd207"))),
+      message = "lalala",
+      badge = None,
+      sound = None,
+      destinations = List(DeviceNotifications.MobileDestination)
+    )
 
   c.actor ! InactiveDevices.GetInactiveDevicesRequest
 
