d341ec73c14970a6a67821fe8db0f5cb98c515af
Yury <Yury@localhost>
2015-12-18 12:27:46 +0300

    Quick fix for client bug.

--- questd/app/models/domain/user/profile/Analytics.scala
+++ questd/app/models/domain/user/profile/Analytics.scala
@@ -6,6 +6,6 @@ import java.util.Date
  * All information what is required by analytics.
  */
 case class Analytics(
-  profileCreationDate: Date = new Date(),
+  profileCreationDate: Date = new Date(new Date().getTime - 24*60*60*1000), // TODO: set new date here.
   source: Map[String, String] = Map.empty)
 
