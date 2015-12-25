f00e71b84133441d4da6676e3619564b49168ae0
Anatoliy <_tikker_@mail.ru>
2015-12-25 13:57:44 +0300

    Starting script for questme for win - fix.

--- questd/run_server_questme_dev.bat
+++ questd/run_server_questme_dev.bat
@@ -1,5 +1,5 @@
-set SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set SCRIPT_DIR=%cd%
 set CONF=%SCRIPT_DIR%/conf
 set STAGE=%SCRIPT_DIR%/target/universal/stage
 
-%STAGE%/bin/questd -d %SCRIPT_DIR% -Dhttp.port=9000 -Dlogger.file=%CONF%/application-logger.xml -Dconfig.file=%CONF%/application-questme.conf
+%STAGE%/bin/questd -Dhttp.port=9000 -Dlogger.file=%CONF%/application-logger.xml -Dconfig.file=%CONF%/application-questme.conf
